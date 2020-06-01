bd7cb2608425c5be14deeba898e265feb8ae8b0d
Fabiano Araujo <63600082+JCNazaz@users.noreply.github.com>
2020-06-01 19:32:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+/* Game Value */
+
+let min = 1,
+  max = 10,
+  winningNum = getRandomNum(min, max),
+  guessesLeft = 3;
+
+/* UI Elements */
+const game = document.getElementById("game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+/* Assign UI min and max */
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+/* Play Again */
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
+/* Listen Guess */
+
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  /* Validate */
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please, enter a number betwen ${min} and ${max}`, "red");
+  } else if (guess === winningNum) {
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  } else {
+    guessesLeft -= 1;
+    /* Wrong Number */
+    if (guessesLeft === 0) {
+      gameOver(
+        false,
+        `Game Over, you lost. The correct number was ${winningNum}`
+      );
+    } else {
+      guessInput.style.borderColor = "red";
+      guessInput.value = "";
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, "red");
+    }
+  }
+});
+
+/* Game OVer */
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  message.style.color = color;
+  setMessage(msg);
+  guessBtn.value = "Play Again";
+  guessBtn.className = "play-again";
+}
+/* Get Winning Number */
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/* Set Message */
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
